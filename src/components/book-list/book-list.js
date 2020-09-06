13e211295da55e175e8c1f2c03852346b868dd9e
Igor Ovcharenko <KramerJohn@ukr.net>
2020-09-06 10:42:30 +0300

    action dispatch

--- src/components/book-list/book-list.js
+++ src/components/book-list/book-list.js
@@ -1,8 +1,20 @@
 import React, {Component} from "react";
 import BookListItem from "../book-list-item";
-import './book-list.css'
+import './book-list.css';
+import {connect} from 'react-redux';
+import {withBookstoreService} from "../hoc";
+import {booksLoaded} from "../../actions";
+import {compose} from "../../utils";
 
 class BookList extends Component {
+
+    componentDidMount() {
+
+        const {bookstoreService} = this.props;
+        const data = bookstoreService.getBooks();
+
+        this.props.booksLoaded(data)
+    }
 
     render() {
         const {books} = this.props;
@@ -22,5 +34,15 @@ class BookList extends Component {
     }
 }
 
-export default BookList
+const mapStateToProps = ({books}) => {
+    return {books}
+};
+
+const mapDispatchToProps = {booksLoaded}
+
+export default compose(
+    withBookstoreService(),
+    connect(mapStateToProps, mapDispatchToProps)
+)(BookList)
+
 
